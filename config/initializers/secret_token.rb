d75cfc8aa33d261aee3bd7bfc89a1f2d0b1d0dcc
mumo.c <carlos.muniz@crowdint.com>
2010-12-28 12:46:56 -0600

    Initial Commit Test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'ac0b248c98c6a8478c2effff84d3aa3dd7dc67fa21dc07c9f87d5ce1cc6585fd39ed43d974f4fe8449c30db31feb8c238d2b1c3b13e101f222003875c5fe3b79'
